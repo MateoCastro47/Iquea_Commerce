791eaffc055ba6befc1dcc630f015648c0b34cfc
MateoCastro47 <castrosoutomateo@gmail.com>
2026-01-16 12:08:40 +0100

    Parte backend del proyecto

---
+++ Iqüea_back/src/test/java/com/edu/mcs/Iquea/IqueaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.mcs.Iquea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IqueaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
