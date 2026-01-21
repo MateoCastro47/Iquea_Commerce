de3790b768ba67ea4bb44bbc08b8d9971ab72a28
MateoCastro47 <castrosoutomateogmail.com>
2026-01-21 20:54:12 +0100

    Creado servicio de Producto

---
+++ Iqüea_back/src/main/java/com/edu/mcs/Iquea/services/IProductoService.java
@@ -0,0 +1,20 @@
+package com.edu.mcs.Iquea.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.edu.mcs.Iquea.models.Producto;
+import com.edu.mcs.Iquea.models.dto.detalle.ProductoDetalleDTO;
+
+public interface IProductoService {
+    
+    Producto crearProducto(ProductoDetalleDTO dto);
+
+    Optional<Producto> obtenerProductoPorId(Long id);
+
+    List<Producto> obtenertodoslosproductos();
+
+    Producto actualizarProducto(String sku, ProductoDetalleDTO dto);
+
+    void borrarProducto(Long id);
+}
---
+++ Iqüea_back/src/main/java/com/edu/mcs/Iquea/services/implementaciones/ProductoServiceImpl.java
@@ -0,0 +1,72 @@
+package com.edu.mcs.Iquea.services.implementaciones;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.edu.mcs.Iquea.mappers.ProductoMapper;
+import com.edu.mcs.Iquea.models.Producto;
+import com.edu.mcs.Iquea.models.dto.detalle.ProductoDetalleDTO;
+import com.edu.mcs.Iquea.repositories.ProductoRepository;
+import com.edu.mcs.Iquea.services.IProductoService;
+
+public class ProductoServiceImpl implements IProductoService{
+    
+    private final ProductoRepository productoRepository;
+    private final ProductoMapper productoMapper;
+
+    public ProductoServiceImpl(ProductoMapper productoMapper, ProductoRepository productoRepository) {
+        this.productoMapper = productoMapper;
+        this.productoRepository = productoRepository;
+    }
+
+    @Override
+    @Transactional
+    public Producto actualizarProducto(String sku, ProductoDetalleDTO dto) {
+        try{
+        Producto productoActualizado = productoRepository.findBySku(sku)
+        .orElseThrow(() -> new RuntimeException("Producto no encontrado con Sku " + sku));
+
+        productoMapper.updatefromEntity(dto, productoActualizado);
+
+        return productoRepository.save(productoActualizado);
+        }catch(IllegalArgumentException e){
+            throw new RuntimeException("Datos del producto no validos");
+        }
+    }
+
+    @Override
+    @Transactional
+    public void borrarProducto(Long id) {
+        if(productoRepository.existsById(id)){
+            productoRepository.deleteById(id);
+        }else{
+            throw new RuntimeException("El producto con id " + id + " no existe");
+        }
+    }
+
+    @Override
+    @Transactional
+    public Producto crearProducto(ProductoDetalleDTO dto) {
+        if (productoRepository.existsBySku(dto.getSku())) {
+            throw new IllegalArgumentException("Ya existe un producto con el SKU: " + dto.getSku());
+        }
+        Producto producto = productoMapper.toEntity(dto);
+        return productoRepository.save(producto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Producto> obtenerProductoPorId(Long id) {
+        return productoRepository.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> obtenertodoslosproductos() {
+        return productoRepository.findAll();
+    }
+
+    
+}
