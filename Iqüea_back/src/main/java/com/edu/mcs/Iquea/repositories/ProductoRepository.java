f969ae0822a02d6c8227317139d32370b7cd42d5
MateoCastro47 <castrosoutomateogmail.com>
2026-01-23 17:56:31 +0100

    Añadidos filtros en el repositorio de Producto

--- Iqüea_back/src/main/java/com/edu/mcs/Iquea/repositories/ProductoRepository.java
+++ Iqüea_back/src/main/java/com/edu/mcs/Iquea/repositories/ProductoRepository.java
@@ -1,5 +1,7 @@
 package com.edu.mcs.Iquea.repositories;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +12,12 @@ public interface ProductoRepository extends JpaRepository<Producto, Long> {
 
     boolean existsBySku(String sku);
     Optional<Producto> findBySku(String sku);
+
+    List<Producto> findbyNombreContainingCase(String nombre);
+
+    List<Producto> findbyCategoriaId(Long categoriaId);
+
+    List<Producto> findbyes_destacado(Boolean es_destacado);
+
+    List<Producto> findbyPrecioCantidadBetween(BigDecimal precioMinimo, BigDecimal precioMaximo);
 }
