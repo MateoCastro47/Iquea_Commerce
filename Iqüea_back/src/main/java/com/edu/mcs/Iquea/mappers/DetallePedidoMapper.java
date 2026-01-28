79f05044ebaa32045f2219adcbe8697dd14ed9f9
MateoCastro47 <castrosoutomateo@gmail.com>
2026-01-28 11:54:03 +0100

    Creada funcion de calcular subtotal

--- Iqüea_back/src/main/java/com/edu/mcs/Iquea/mappers/DetallePedidoMapper.java
+++ Iqüea_back/src/main/java/com/edu/mcs/Iquea/mappers/DetallePedidoMapper.java
@@ -1,8 +1,10 @@
 package com.edu.mcs.Iquea.mappers;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import com.edu.mcs.Iquea.models.Detalle_pedido;
@@ -11,6 +13,7 @@ import com.edu.mcs.Iquea.models.dto.detalle.DetallePedidoDTO;
 @Mapper(componentModel = "spring", uses = {ProductoMapper.class, PedidoMapper.class})
 public interface DetallePedidoMapper {
 
+    @Mapping(target = "subtotal", expression = "java(calcularSubTotal(detalle_pedido))")
     DetallePedidoDTO toDTO(Detalle_pedido detallePedido);
 
     Detalle_pedido toEntity(DetallePedidoDTO dto);
@@ -19,4 +22,10 @@ public interface DetallePedidoMapper {
 
     void updateFromEntity(DetallePedidoDTO dto, @MappingTarget Detalle_pedido detallePedido);
 
+    default BigDecimal calcularSubTotal(Detalle_pedido detalle_pedido){
+        if(detalle_pedido.getPrecioUnitario() == null || detalle_pedido.getCantidad() == null){
+            return BigDecimal.ZERO;
+        }
+        return detalle_pedido.getPrecioUnitario().multiply(new BigDecimal(detalle_pedido.getCantidad()));
+    }
 }
