65075d4e4acf9a97aae59c60b76f4dc0cb66f1f1
MateoCastro47 <castrosoutomateo@gmail.com>
2026-01-28 11:53:41 +0100

    Actualizado repositorio de categoria

--- Iqüea_back/src/main/java/com/edu/mcs/Iquea/repositories/CategoriasRepository.java
+++ Iqüea_back/src/main/java/com/edu/mcs/Iquea/repositories/CategoriasRepository.java
@@ -1,9 +1,18 @@
 package com.edu.mcs.Iquea.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edu.mcs.Iquea.models.Categorias;
 
-public interface CategoriasRepository extends JpaRepository<Categorias, Long> {
 
+
+public interface CategoriasRepository extends JpaRepository<Categorias, Long> {
+    
+    boolean existsByNombre(String nombre);
+
+    void deleteByNombre(String nombre);
+    
+    Optional<Categorias> findByNombre(String nombre);
 }
