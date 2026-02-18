f7408f5305e4a5cc3972d13fe8d5a42f0f7e05a1
MateoCastro47 <castrosoutomateogmail.com>
2026-02-18 20:18:28 +0100

    Actualizado mapper para que no devuelva el hash en el json

--- Iqüea_back/src/main/java/com/edu/mcs/Iquea/mappers/UsuarioMapper.java
+++ Iqüea_back/src/main/java/com/edu/mcs/Iquea/mappers/UsuarioMapper.java
@@ -3,6 +3,7 @@ package com.edu.mcs.Iquea.mappers;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import com.edu.mcs.Iquea.models.Usuario;
@@ -10,7 +11,9 @@ import com.edu.mcs.Iquea.models.dto.detalle.UsuarioDetalleDTO;
 
 @Mapper(componentModel = "spring")
 public interface  UsuarioMapper {
-    
+
+    // No devolver el hash de la contraseña en las respuestas JSON
+    @Mapping(target = "password", ignore = true)
     UsuarioDetalleDTO toDTO(Usuario usuario);
 
     List<UsuarioDetalleDTO> toDTOlist(List<Usuario> usuarios);
